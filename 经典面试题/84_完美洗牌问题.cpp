b38026a41a5770b238ec79e9e0d9594edf9251be
ZYBaisichen <bhbaisichen@gmail.com>
2024-04-07 15:38:55 +0800

    84-85

---
+++ 经典面试题/84_完美洗牌问题.cpp
@@ -0,0 +1,160 @@
+/*
+ * @Author: baisichen
+ * @Date: 2024-02-26 15:02:38
+ * @LastEditTime: 2024-04-07 11:58:37
+ * @LastEditors: baisichen
+ * @Description:
+ */
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+#include <set>
+#include <ctime>
+#include <cstdlib>
+#include <map>
+#include <limits.h>
+#include <deque>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <functional>
+
+using namespace std;
+
+/*
+给定一个长度为偶数的数组arr，长度记为2*n。前N个为左部分，后N个为右部分。arr就可以表示为{l1,l2,..ln,r1,r2,...rn}，
+将数组调整成{r1,l1,r2,l2,...,rn,ln}的样子
+要求使用有限几个变量，复杂度O(n)
+*/
+/*
+
+1. 函数f根据i原来的位置，和一半长度n。返回应该在调整后来到的位置。int f(i,n);
+假设[1 2 3 4 5 6 7 8 ]，此时n=4。
+1. 当i>n时，来到的位置是(i-N)*2-1。将其调整到左半区，然后到奇数位置
+2. 当i<=n时，来到的位置是i*2
+
+2. 如果不使用额外空间，则需要使用下标循环怼方式：
+[a b c d]
+[1 2 3 4]
+假设数组abcd的位置分别是1234，先计算a需要来到的位置是2，则空降2位置。将b怼出来，b将要来到的位置是2*2=4，则空降4位置。
+将d怼出来，d将要来到的位置是(4-2)*2-1=3位置，空降3位置，将c怼出来；c将要来到(3-2)*2-1位置，将c填到1位置
+
+3. 但并不是所有的数组都是一个循环。比如:
+[a b c d e f]
+[1 2 3 4 5 6]
+a来到2位置，b来到d位置，d来到1位置形成一个循环。有个问题是怎么在有限变量下，找到所有的环？
+
+4. 完美洗牌问题的结论：当数组长度是s=3^k-1（偶数,k>0）时，下标循环怼的环入口是1,3,9,....3^(k-1)
+
+5. 如果s不正好等于3^(k-1)，可以使用凑数的方式来搞。在此之前需要先了解一个算法原型：arr=[l1,l2,l3,r1,r2]，将数组arr调整成[r1,r2,l1,l2,l3]。
+流程是将l1,l2,l3逆序，r1,r2逆序，然后让整体逆序：[l3,l2,l1,r2,r1]->[r1,r2,l1,l2,l3]
+
+6. 假设s=14, 数组为arr=[l1,l2,l3,l4,l5,l6,l7,r1,r2,r3,r4,r5,r6,r7]。
+ a. 取最接近14的3^k-1为8，则在右边取8/2=4个数和左边的l1...l4放在一起做下标循环怼。将[l5,l6,l7,r1,r2,r3,r4]做完美整逆序。arr变成[l1,l2,l3,l4,r1,r2,r3,r4,l5,l6,l7,r5,r6,r7]
+ b. 左边做下标循环怼，开始位置分别是l1,l3。右边s=6，再取3^k-1=2, 做逆序之后变成[l1,l2,l3,l4,r1,r2,r3,r4, l5,l6,r5,r6,l7,r7]
+ c. 最后l6,l7刚好还剩2个是3^1-1，自己做下标循环怼
+
+7. 复杂度估计：假设每次取来做下标循环怼的数是a=3^k-1, 则用来和a/2组合做下标循环怼的左边的数不会超过3a，因为如果超过的话k的值将可以取到+1的数。所以每次逆序的复杂度是O(a)， 下标循环怼的复杂度是O(a)。
+   再下次的复杂度是O(b),O(c).....，而a+b+c+....=N。所以总复杂度是O(N)
+*/
+
+class Solution
+{
+public:
+
+
+    //主函数
+    //7,8,9,10
+   void shuffle_main(vector<int>& arr) {
+        shuffle(arr, 0, arr.size()-1);
+   }
+   //在l和r上做完美洗牌问题
+   void shuffle(vector<int>& arr, int l, int r) {
+        while (r-l+1>0) {//只要[l...r]上还剩余数字及一直处理
+            int len = r-l+1;
+            int base = 3;
+            int k=1;
+            //计算小于等于len，离len最近的满足3^k-1的数
+            //即满足3^k-1<=len -> 3^k <= len+1
+            while (base <= (len+1)/3) { //防止溢出
+                base*=3;
+                k++;
+            }
+
+            //base=3^k, 需要解决base-1个数
+            int half = (base-1)/2;
+            int mid = (l+r)/2; //总数组的中点位置. 上中点
+
+            //需要旋转的左部分为[l+half...mid]，右部分为[mid+1, half+mid]
+            rotate(arr, l+half, mid, half+mid);
+
+            // //以l为开始，长度为base-1的数组上做下标循环怼
+            cycles(arr, l, base-1, k);
+            l += base-1;
+        }
+   }
+   //[l..m]为左部分，[m+1...r]是右部分。左右两部分互换
+   void rotate(vector<int> &arr, int l, int m, int r)
+   {
+        reverse(arr, l, m);
+        reverse(arr, m + 1, r);
+        reverse(arr, l, r);
+   }
+   //对l和r范围上做逆序
+   void reverse(vector<int>& arr, int l, int r) {
+       while (l<r) {
+           int tmp = arr[r];
+           arr[r--] = arr[l];
+           arr[l++] = tmp;
+       }
+   }
+    //输入老下标，输出新下标
+   int modify_index(int i, int len) {
+        cout << endl << "modify_index:" << i << " len:" << len << endl;
+        if (i > (len/2)) {
+            return (i-len/2)*2-1;
+        } else {
+            return i*2;
+        }
+   }
+   //从start开始，往右len长度这一段，做下标连续推
+   //出发位置依次是1,3,9
+   void cycles(vector<int>& arr, int start, int len, int k) {
+        cout <<  "start:" << start << " len:" << len << " k:" << k << endl;
+       for (int i=0,trigger=1;i<k;i++,trigger*=3) { //枚举环开始的位置， k-1个开始点
+           int s_idx = start+trigger-1;
+           int pre_val = arr[s_idx];
+           int cur_need_idx = modify_index(s_idx-start+1, len)+start-1;
+           cout << "s_idx:" << s_idx << " cur_need_idx:" << cur_need_idx << endl;
+           while (cur_need_idx != s_idx) {
+               int tmp = arr[cur_need_idx];
+               arr[cur_need_idx] = pre_val;
+               pre_val = tmp;
+               int old_need_idx = cur_need_idx;
+               
+               cur_need_idx = modify_index(cur_need_idx-start+1, len)+start-1;//计算时换算到从1开始下标
+               cout << "old_need_idx:" << old_need_idx << " cur_need_idx:" << cur_need_idx << " len:" << len  << endl;
+           }
+           //最后将最后一个数放在开始的位置
+           arr[s_idx] = pre_val;
+       }
+   }
+};
+
+
+int main()
+{
+
+    Solution sol;
+    // string s = "aba";
+    // vector<int> arr = {3,4,5,6,7,15,16,17,19,20};
+    // vector<int> arr = {3,4};
+    vector<int> arr = {1,2,3,4,5,6,7,8};
+    sol.shuffle_main(arr);
+    for (auto iter:arr) {
+        cout << iter << " ";
+    }
+    cout << endl;
+    return 0;
+}
---
+++ 经典面试题/85_环形山问题.cpp
@@ -0,0 +1,292 @@
+/*
+ * @Author: baisichen
+ * @Date: 2024-02-26 15:02:38
+ * @LastEditTime: 2024-04-07 15:37:43
+ * @LastEditors: baisichen
+ * @Description:
+ */
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+#include <set>
+#include <ctime>
+#include <cstdlib>
+#include <map>
+#include <limits.h>
+#include <deque>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <sstream>
+
+using namespace std;
+
+/*
+单调栈：
+
+给出这样一座环形山，哪两座山的烽火可以相互看见呢？
+{3,1,2,4,5}和{1,2,4,5,3}是相同结构环形山
+
+必须满足如下两个标准：
+
+1.如果两座山相邻，相邻必定能相互看见。
+
+1既可以顺时针也可逆时针走到4。如果两座山峰不相邻，那么必然有两条路。
+
+这两条路中，如果某一条路上出现了所有的值，比1和4的最小值 都不大，则1和4可以相互看见。
+比如1到4两条路，都存在比1大的值，所以1和4之间的烽火互相看不见。
+
+2.如果两座山不相邻，则两座山 之间有两条路，假设两座山的最小值是min
+如果某一条路上出现了所有的值，都比不大于min(<=min)
+
+https://blog.csdn.net/Mr_zhang66/article/details/110392555
+
+进阶：如果环形山中，含有相同高度的山，有多少对可见山峰
+*/
+
+/*
+[3,1,2,4,5]，相邻的有5对，还有[3,4],[2,3]，共7对。
+
+一、无重复值。O(1)。为了去重，规定只从小的数找大的数。
+1. 如果数组长度len=1，只有0对
+2. 2个数，有1对
+3. 3个数，两两相邻，共3对
+4. n个数，且n个数不重。一定能找到最大值max和次大值。任何位置x，顺时针一定找到可以找到第一个比x大的数y，同样在逆时针方向上可以找到第一个比x大的数z。
+则一定可以找到[x,y]和[x,z]，所以x如果不是最大值和次大值，则一定可以找到两对。所以所有的x都可以找到2对，共2*(n-2)对。
+最后一对是[次大值,次大值], 总对数是2*(n-1)+1=2*n-3
+
+二、有重复值
+1. 先遍历arr找到最大值max，假设为5
+2. 从max触发逆时针出发，最后回到5. 准备一个栈，元素组织是[value, 次数]， 要求栈顶到栈底按照value从小到大
+3. 当遇到比栈顶大的元素，栈顶弹出时。假设栈顶是3个4，在下面是2个5，任何一个4在逆时针和顺时针方向上都能找到一个5，结算3*2=6对。3个4内部两两可见，结算c(3,2)=3对。共结算9对
+4. 假设从max遍历之后，栈中还有数，就需要来到清算阶段。比如{5,4,3,2}，则栈中{{2,1},{3,1},{4,1},{5,1}}。
+   a. 当栈大小>2时：栈顶元素为{2,k}，则顺时针和逆时针方向每个2都可以找到一对，然后2内部可以找到c(k,2)对
+   b. 栈大小刚好等于2时：栈底max只有一个呢，{{4,k},{5,1}}，顺时针或者逆时针方向遇到的是同一个数，则对外产生k*1对，内部产生c(k,2)对；
+      {{4,k},{5,m}},栈底有多个，则每个4产生2对，内部产生c(k,2)对
+   c. 栈大小为1时：只会内部产生可见对。{{max,k}}，产生c(k,2)对。
+5. 之所以要从最大值开始遍历，是因为要为栈打底，保证转一圈有值
+
+每个数最多进栈一次，出栈一次
+*/
+
+class Solution
+{
+public:
+    
+    struct Node {
+        int val;
+        int times;
+        Node(int v) {
+            val = v;
+            times=1;
+        }
+    };
+    void print_st(stack<Node>& st, string suf) {
+        cout << suf << endl;
+        stack<Node> st_tmp;
+        while (!st.empty()) {
+            cout << st.top().val << "," << st.top().times << " ";
+            st_tmp.push(st.top());
+            st.pop();
+        }
+        cout << endl;
+        while (!st_tmp.empty()) {
+            st.push(st_tmp.top());
+            st_tmp.pop();
+        }
+    }
+    int get_visible_num(vector<int>& arr) {
+        int len = arr.size();
+        if (len < 2) {
+            return 0;
+        }
+
+        int max_idx = 0;
+        for (int i = 1; i < len; i++) {
+            max_idx = arr[max_idx] > arr[i] ? max_idx : i;
+        }
+        // cout << "max_idx=" << max_idx << endl;
+        
+        stack<Node> st; //栈顶到栈底，递增
+        st.push(Node(arr[max_idx]));
+        // cout <<  "start_top:"<< st.top().val << endl;
+        // return 0;
+        int idx = next_idx(max_idx, len);
+        // cout << "遍历阶段开始======" << endl;
+        // cout << "idx:" << idx << " " << endl;
+        int ans = 0;
+        //回到最大值位置停止
+        for (;idx!=max_idx;) {
+            /*
+            当前的数要进入栈，如果栈顶比当前值小，则弹出结算
+            因为栈底是最大值，所以第二个数入的时候肯定不会弹出下面的数，所以从第二个数弹出时外面一定可以产生两对
+            */
+           while (st.top().val < arr[idx]) {
+                int k = st.top().times;
+                st.pop();
+                ans += k*2;
+                ans += get_inter_sum(k);
+           }
+           
+
+           if (st.top().val == arr[idx]) {
+               Node& cur_node = st.top();
+               st.pop();
+               cur_node.times++;
+               st.push(cur_node);
+           } else {
+               st.push(Node(arr[idx]));
+           }
+           idx = next_idx(idx, len);
+        //    cout << "=====" << endl;
+        //    cout << "idx:" << idx << " " <<endl;
+        }
+        // print_st(st, "遍历结束栈状态");
+        // cout << endl << "遍历阶段结束====== ans:" << ans << endl;
+
+        //处理栈内剩余的部分
+        while (!st.empty()) {
+            int st_s = st.size();
+            int k = -1;
+            if (st_s > 2) { //清算栈大小>2的情况
+                k = st.top().times;
+                st.pop();
+                ans += k*2;
+                ans += get_inter_sum(k);
+            } else if (st_s==2) { //清算栈中有2条记录的情况
+                Node& top_node = st.top();
+                k = top_node.times;
+                st.pop();
+                int last_times = st.top().times;
+                if (last_times == 1) {
+                    ans += k;
+                } else {
+                    ans += k*2;
+                }
+                ans += get_inter_sum(k);
+            } else { //只剩一个数了
+                k = st.top().times;
+                st.pop();
+                ans += get_inter_sum(k);
+            }
+        }
+
+        return ans;
+    }
+    //或者c(k,2)，从k个里面选2个
+    int get_inter_sum(int k) {
+        return k*((k-1)/2);
+    }
+
+    //顺时针
+    int next_idx(int i, int len) {
+        if (i==len-1) {
+            i=0;
+        } else {
+            i++;
+        }
+        return i;
+    }
+    //逆时针
+    int last_idx(int i, int len) {
+        if (i==0) {
+            i=len-1;
+        } else {
+            i--;
+        }
+        return i;
+    }
+
+    //暴力
+    int get_visibale_num_baoli(vector<int>& arr) {
+        int len = arr.size();
+        set<string> sets;
+        int ans = 0;
+        for (int i=0;i<len;i++) { //枚举开始
+            //从每个位置开始，利用小找大原则，看能找到多少对
+
+            int num = 0;
+            for (int j=0;j<len;j++) {
+                if (i!=j) { //不找自己
+                    if (arr[i] == arr[j]) {
+                        std::ostringstream oss;
+                        // oss << min(i,j) << "_" << max(i,j); //以下标为key，小在前面，大在后面
+                        std::string key = oss.str();
+                        if (sets.find(key) != sets.end() && is_visible(arr, i, j)) { // 可见
+                            num++;
+                            sets.insert(key);
+                        }
+                    } else if (is_visible(arr, i, j)){
+                        num++;
+                    }
+                }
+            }
+            
+            ans += num;
+        }
+        return ans;
+    }
+
+    bool is_visible(vector<int>& arr, int l_idx, int h_idx) {
+        // cout << "l_idx " << l_idx << " h_idx " << h_idx << endl;
+        if (arr[l_idx] > arr[h_idx]) {
+            return false;
+        }
+        int len = arr.size();
+
+        //顺时针
+        bool next_walk = true;
+        int cur = next_idx(l_idx, len);
+        while (cur != h_idx) {
+            // cout << "l_idx " << l_idx << " h_idx " << h_idx << " next_walk_cur:" << cur << endl;
+            if (arr[cur] > arr[l_idx]) {
+                next_walk = false;
+            }
+            cur = next_idx(cur, len);
+        }
+
+        //逆时针
+        cur = last_idx(l_idx, len);
+        bool last_walk = true;
+        while (cur!=h_idx) {
+            // cout << "l_idx " << l_idx << " h_idx " << h_idx << " last_walk_cur:" << cur << endl;
+            if (arr[cur] > arr[l_idx]) { //遇到h_idx数之前，遇到了比l_idx大的，则l_idx和h_idx不可见
+                last_walk = false;
+            }
+            cur = last_idx(cur, len);
+        }
+
+        return next_walk || last_walk;//有一个方向可见，就是可见的
+    }
+
+};
+
+int main()
+{
+
+    Solution sol;
+
+    int test_time = 10000;
+    int max_len = 10;
+    int max_num = 1000;
+    for (int i = 1; i <= test_time; i++) {
+        int len = (int) (rand() % max_len) + 1;
+        vector<int> arr(len, 0);
+        for (int j = 0; j < len; j++) {
+            arr[j] = rand() % max_num + 1;
+        }
+        int ans1 = sol.get_visibale_num_baoli(arr);
+        int ans2 = sol.get_visible_num(arr);
+        if (ans1 != ans2)
+        {
+            cout << "ans1:" << ans1 << " ans2:" << ans2 << endl;
+        }
+    }
+
+    // vector<int> arr = {3,1,2,4,5};
+    // cout << sol.get_visibale_num_baoli(arr) << endl;
+    // cout << sol.get_visible_num(arr) << endl;
+    return 0;
+}
