2b91c6353d29f0be5be855045c4e65828b5abecf
ZYBaisichen <bhbaisichen@gmail.com>
2024-06-25 11:05:05 +0800

    add

--- leetcode热题/621_任务调度器.cpp
+++ leetcode热题/621_任务调度器.cpp
@@ -66,12 +66,51 @@ tasks[i] 是大写英文字母
 */
 
 /*
-
+来自题解：https://leetcode.cn/problems/task-scheduler/solutions/196302/tong-zi-by-popopop/
+桶的思想
+1. 建立大小为n+1的桶（因为任务本身要消耗1时间），个数是任务数量最多的那个任务。比如n=2，重复任务数最多的是A有6个, 则建立6个桶，每个容量为3，我们可以把每个桶看做一轮任务
+    A - -
+    A - -
+    A - -
+    A - -
+    A - -
+    A - -
+2. 最简单的，如果没有其他任务，我们完成任务A的时间应该是(6-1)*3+1=16. 最后一个桶结束后不需要再等待了。
+3. 接下来我们添加些其他任务，当加入C时，对总时间没有产生影响，因为C全在冷却时间内；加入B后，B的数量和A相同，这就导致了最后一个桶需要多执行一个任务，总时间是hi(6-1)*3+2=17
+    A B C
+    A B C
+    A B
+    A B
+    A B
+    A B
+4. 总结下来，总时间=(桶个数-1)*(n+1)+最后一个桶的任务数
+5. 当冷却时间短，任务种类多时，需要临时在每个桶内加入新的元素。比如加入了F，同样满足冷却时间。可以认为此时，所有的桶都填满了，所有任务都紧凑的放在一起执行。
+   总时间就是任务的大小len
+    A B C F
+    A B C F
+    A B C
+    A B C
+    A B C 
+    A B
+6. 4、5两种情况取最大值就可以。因为当没有填满桶时，总时间一定比len小
 */
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        
+        vector<int> c_cnt(26, 0);
+        int len = tasks.size();
+        for (int i=0;i<len;i++) {
+            c_cnt[tasks[i]-'A']++;
+        }
+        sort(c_cnt.rbegin(), c_cnt.rend()); //倒序排列
+        int max_num = c_cnt[0];
+        int max_cnt = 0;//任务数量最大的数的统计。也代表着最后一个桶的任务数量
+        int idx = 0;
+        while (idx<c_cnt.size() && c_cnt[idx] == max_num) {
+            max_cnt++;
+            idx++;
+        }
+        return max(len, (max_num-1)*(n+1)+max_cnt);
     }
 };
 
